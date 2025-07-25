16e2f9a82568733d635159b1ef001354e5dbaf0a
Leandro Martini <martinileandro12@gmail.com>
2025-07-25 19:47:46 -0300

    compartilhamento de dados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first-component/first-component';
+import { ParentData } from './components/parent-data/parent-data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponent],
+  imports: [RouterOutlet,FirstComponent,ParentData],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('firstProject-Angular');
+
+  userName:string = "Leandro";
+  userData = {email: "leandro@email.com", idade: 20};
 }
