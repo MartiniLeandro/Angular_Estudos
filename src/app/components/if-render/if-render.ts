400351137091a52f6fa171b61795cc23b2f5f593
Leandro Martini <martinileandro12@gmail.com>
2025-07-26 15:17:42 -0300

    ajeitando o html por aula

---
+++ src/app/components/if-render/if-render.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  imports: [],
+  templateUrl: './if-render.html',
+  styleUrl: './if-render.css'
+})
+export class IfRender {
+
+}
