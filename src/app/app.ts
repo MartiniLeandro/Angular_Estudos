f0f4459fb728b8aaff633fd01f898b37d5e1fa49
Leandro Martini <martinileandro12@gmail.com>
2025-07-28 20:56:36 -0300

    passando eventos do filho para o pai

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { ParentData } from './components/parent-data/parent-data';
 import { Directives } from './components/directives/directives';
 import { IfRender } from './components/if-render/if-render';
 import { Eventos } from "./components/eventos/eventos";
+import { Emitter } from './components/emitter/emitter';
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponent, ParentData, Directives, IfRender, Eventos],
+  imports: [FirstComponent, ParentData, Directives, IfRender, Eventos,Emitter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
