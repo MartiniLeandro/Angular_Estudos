93b70c99731e67d0b2393426ca04546704ebc177
Leandro Martini <martinileandro12@gmail.com>
2025-08-01 19:48:09 -0300

    add weigth in interface

--- src/app/components/list-hander/list-hander.ts
+++ src/app/components/list-hander/list-hander.ts
@@ -10,9 +10,9 @@ import { Animal } from '../../Animal';
 })
 export class ListHander {
   animals: Animal[] = [
-    {name:"Turca", type:"Dog",age:7},
-    {name:"Tom", type:"Cat", age:3},
-    {name:"Frida", type:"Cat", age: 4},
-    {name:"Bob", type:"Dog", age:2},
+    {name:"Turca",weight:"7kg", type:"Dog",age:7},
+    {name:"Tom",weight:"5kg", type:"Cat", age:3},
+    {name:"Frida",weight:"2kg", type:"Cat", age: 4},
+    {name:"Bob",weight:"9kg", type:"Dog", age:2},
 ]
 }
