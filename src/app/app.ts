3c64add72885c8685412cb5cb6329b44cd850d0b
Leandro Martini <martinileandro12@gmail.com>
2025-08-02 12:46:34 -0300

    utilizando two way data bind

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FirstComponent } from './components/first-component/first-component';
 import { ParentData } from './components/parent-data/parent-data';
 import { Directives } from './components/directives/directives';
@@ -7,10 +8,11 @@ import { Eventos } from "./components/eventos/eventos";
 import { Emitter } from './components/emitter/emitter';
 import { ListHander } from "./components/list-hander/list-hander";
 import { PipeOperator } from "./components/pipe-operator/pipe-operator";
+import { TwoWayBinding } from "./components/two-way-binding/two-way-binding";
 
 @Component({
   selector: 'app-root',
-  imports: [FirstComponent, ParentData, Directives, IfRender, Eventos, Emitter, ListHander, PipeOperator],
+  imports: [FirstComponent, ParentData, Directives, IfRender, Eventos, Emitter, ListHander, PipeOperator, TwoWayBinding, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
