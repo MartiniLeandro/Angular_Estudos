fd50fe6d591ddf915b686da6720015f05658aa3a
Leandro Martini <martinileandro12@gmail.com>
2025-08-05 09:04:36 -0300

    ajustando estrutura do repositório

--- src/app/Animal.ts
+++
@@ -1,6 +0,0 @@
-export interface Animal {
-    name:string,
-    weight:string,
-    type:string,
-    age:number;
-}
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { FirstComponent } from './components/first-component/first-component';
-import { ListHander } from './components/list-hander/list-hander';
-
-export const routes: Routes = [
-    {path:'', component:FirstComponent},
-    {path:'list', component:ListHander}
-];
