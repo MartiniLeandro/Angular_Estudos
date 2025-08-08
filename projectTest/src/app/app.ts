9ee71d58797059391f51005d3c11e6317262b97f
Leandro Martini <martinileandro12@gmail.com>
2025-08-07 21:47:18 -0300

    começando um projeto teste

--- projectTest/src/app/app.ts
+++ projectTest/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { FirstPage } from "./components/first-page/first-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, FirstPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
