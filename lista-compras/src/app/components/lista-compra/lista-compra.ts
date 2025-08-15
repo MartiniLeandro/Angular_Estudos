f7179e6a951939daf3455950ecc86cef0f6dabdd
Leandro Martini <martinileandro12@gmail.com>
2025-08-15 17:47:42 -0300

    lista de compras completa

--- lista-compras/src/app/components/lista-compra/lista-compra.ts
+++ lista-compras/src/app/components/lista-compra/lista-compra.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ItemList } from './itemList';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compra',
-  imports: [FormsModule],
+  imports: [FormsModule, NgClass],
   templateUrl: './lista-compra.html',
   styleUrl: './lista-compra.scss'
 })
@@ -18,6 +19,14 @@ export class ListaCompra {
     newItem.id = this.items.length + 1
     this.items.push(newItem);
     this.item='';
+  }
+
+  riscarItem(item:ItemList){
+    item.comprado = !item.comprado
+  }
+
+  reset(){
+    this.items = [];
   }
   
 }
