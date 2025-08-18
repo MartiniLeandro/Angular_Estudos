3451c95baba1d2fe3847f9f16a98cbbbb511ce74
Leandro Martini <martinileandro12@gmail.com>
2025-08-18 07:15:10 -0300

    base da navbar feita

--- crud-angular-material/src/app/app.ts
+++ crud-angular-material/src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterOutlet,RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatToolbarModule, RouterOutlet, RouterLink, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
