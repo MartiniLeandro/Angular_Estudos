77f009aee8dd0a93d10ca4fbf6158de2e28218dc
Leandro Martini <martinileandro12@gmail.com>
2025-08-18 08:29:17 -0300

    configurando o form e a classe cliente

--- crud-angular-material/src/app/components/cadastro/cadastro.ts
+++ crud-angular-material/src/app/components/cadastro/cadastro.ts
@@ -6,6 +6,7 @@ import {MatFormField} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button'
+import { Cliente } from './cliente';
 
 
 @Component({
@@ -15,5 +16,9 @@ import {MatButtonModule} from '@angular/material/button'
   styleUrl: './cadastro.scss'
 })
 export class Cadastro {
+  cliente:Cliente = Cliente.newCliente()
 
+  salvar(){
+    console.log(this.cliente);
+  }
 }
