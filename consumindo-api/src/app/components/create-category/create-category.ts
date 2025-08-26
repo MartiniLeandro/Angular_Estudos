95a238db5f3ee482dc969c23510419fd63e684d2
Leandro Martini <martinileandro12@gmail.com>
2025-08-26 20:41:42 -0300

    create category done

--- consumindo-api/src/app/components/create-category/create-category.ts
+++ consumindo-api/src/app/components/create-category/create-category.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormField } from '@angular/material/form-field'
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../service/api.service';
+import { Categorias } from '../../models/categorias';
 
 @Component({
   selector: 'app-create-category',
-  imports: [],
+  imports: [MatFormField,MatSelectModule,MatInputModule,FormsModule],
   templateUrl: './create-category.html',
   styleUrl: './create-category.scss'
 })
 export class CreateCategory {
+  constructor(private apiService:ApiService){}
 
+  nome = "";
+  descricao="";
+
+  criarCategoria(){
+    const newCategoria:Categorias = {nome: this.nome, descricao: this.descricao};
+    this.apiService.postCategorias(newCategoria).subscribe(data => console.log(data));
+    this.nome = "";
+    this.descricao = '';
+  }
 }
