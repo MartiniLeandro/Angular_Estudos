6c08694461283178e6e8993e0e2c240c615633db
Leandro Martini <martinileandro12@gmail.com>
2025-09-03 07:51:40 -0300

    tela de login

--- MyTask-front/src/app/app.routes.ts
+++ MyTask-front/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FirstPage } from './components/first-page/first-page';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
+    {path:'login', component:Login},
     {path:'', component:FirstPage},
 ];
