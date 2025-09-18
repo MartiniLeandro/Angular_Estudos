a87ecb38da78aafa1eb138a37599c54a683b4759
Leandro Martini <martinileandro12@gmail.com>
2025-09-18 17:58:13 -0300

    fazendo aparecer as categories

--- MoneyTrack-front/src/app/components/create-launch/create-launch.ts
+++ MoneyTrack-front/src/app/components/create-launch/create-launch.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../navbar/navbar";
+import { CategoryAPI } from '../../services/category/category.service';
+import { category } from '../../models/categoryData';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-launch',
-  imports: [Navbar],
+  imports: [Navbar, CommonModule],
   templateUrl: './create-launch.html',
   styleUrl: './create-launch.scss'
 })
 export class CreateLaunch {
+  constructor(private categoryAPI:CategoryAPI){}
 
+  categoriesExpense:category[] = [];
+  categoriesRevenue:category[] = [];
+  notShowExpenses:boolean = true
+  notShowRevenues:boolean = true
+
+  ngOnInit(){
+    this.getCategories()
+  }
+
+  getCategories(){
+    this.categoryAPI.getAllCategories().subscribe(datas => {
+      for(let data of datas){
+        if(data.typeValue == "EXPENSE"){
+          this.categoriesExpense.push(data)
+        }else {
+          this.categoriesRevenue.push(data)
+        }
+      }
+
+      console.log(this.categoriesExpense)
+      console.log(this.categoriesRevenue)
+    })
+  }
+
+  showExpenses(){
+    this.notShowExpenses = false
+    this.notShowRevenues = true
+  }
+  showRevenues(){
+    this.notShowRevenues = false;
+    this.notShowExpenses = true
+  }
 }
---
+++ MoneyTrack-front/src/app/services/category/category.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { category } from '../../models/categoryData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryAPI {
+  constructor(private api:HttpClient){}
+
+  urlAPI:string = "http://localhost:8080/categories"
+
+  getAllCategories():Observable<category[]>{
+    return this.api.get<category[]>(this.urlAPI);
+  }
+  
+}
